4f6d3083c05d71d25600e40a91aea1915a5b6bf3
Maysara <Maysara.Elshewehy@gmail.com>
2026-01-15 09:15:58 +0200

    0.1.3

--- src/index.ts
+++ src/index.ts
@@ -905,7 +905,7 @@
             console.warn('[ServerManager] i18n not initialized. Using default value or key.');
             return defaultValue ?? key;
         }
-        return i18n.tLang(lang, key) ?? defaultValue ?? key;
+        return i18n.tLang(key, lang) ?? defaultValue ?? key;
     }
 
 
